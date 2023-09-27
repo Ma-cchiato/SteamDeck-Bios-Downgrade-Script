d83f8341eb52eb6d5d58619abcc70d33371bd228
Ma-cchiato <122413511+Ma-cchiato@users.noreply.github.com>
2023-09-28 02:02:00 +0900

    Update bios.sh

--- bios.sh
+++ bios.sh
@@ -4,7 +4,8 @@ wget https://github.com/Ma-cchiato/deck_bios_downgrade/raw/main/SD_Unlocker -O '
 sudo chmod +x /home/deck/SD_Unlocker
 sudo steamos-readonly disable
 sudo rm -rf /usr/share/jupiter_bios/F7A*.fd
-sudo cp '/home/deck/F7A0110_sign.fd' '/usr/share/jupiter_bios/F7A0116_sign.fd'
+sudo cp '/home/deck/F7A0110_sign.fd' '/usr/share/jupiter_bios/F7A0116_sign.fd' # For SteamOS 3.5 ~ Stable 
+sudo cp '/home/deck/F7A0110_sign.fd' '/usr/share/jupiter_bios/F7A0118_sign.fd' # For SteamOS 3.6 
 sudo steamos-readonly enable
 sudo /home/deck/SD_Unlocker
 sudo /usr/share/jupiter_bios_updater/h2offt '/home/deck/F7A0110_sign.fd'
