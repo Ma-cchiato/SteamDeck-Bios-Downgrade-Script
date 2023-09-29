5db2bff0ac0b57ff0181c8d4704f14fc03600345
Ma-cchiato <122413511+Ma-cchiato@users.noreply.github.com>
2023-09-29 15:41:58 +0900

    Update bios.sh
    Add backup bios

--- bios.sh
+++ bios.sh
@@ -6,6 +6,7 @@ sudo steamos-readonly disable
 sudo rm -rf /usr/share/jupiter_bios/F7A*.fd
 sudo cp '/home/deck/F7A0110_sign.fd' '/usr/share/jupiter_bios/F7A0116_sign.fd' # For SteamOS 3.5 ~ Stable 
 sudo cp '/home/deck/F7A0110_sign.fd' '/usr/share/jupiter_bios/F7A0118_sign.fd' # For SteamOS 3.6 
+sudo /usr/share/jupiter_bios_updater/h2offt '/home/deck/bios_backup.fd' -O # 기존 바이오스 백업, 복구 가이드는 https://gall.dcinside.com/mgallery/board/view/?id=steamdeck&no=91558 참고
 sudo steamos-readonly enable
 sudo /home/deck/SD_Unlocker
 sudo /usr/share/jupiter_bios_updater/h2offt '/home/deck/F7A0110_sign.fd'
