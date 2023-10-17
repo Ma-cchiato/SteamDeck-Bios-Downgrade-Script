4de250181c1a6194b54802f5e1d8f449a336050f
Ma-cchiato <122413511+Ma-cchiato@users.noreply.github.com>
2023-10-17 14:21:38 +0900

    Update enable_bios_update.sh

--- enable_bios_update.sh
+++ enable_bios_update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo systemctl enable jupiter-biosupdate.service
+sudo systemctl unmask jupiter-biosupdate
 sudo steamos-readonly disable
 sudo rm -rf /foxnet
 sudo steamos-readonly enable
