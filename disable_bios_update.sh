596225569cd9af1e78ad1234179620503726426d
Ma-cchiato <122413511+Ma-cchiato@users.noreply.github.com>
2023-10-17 14:21:19 +0900

    Update disable_bios_update.sh

--- disable_bios_update.sh
+++ disable_bios_update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo systemctl disable jupiter-biosupdate.service
+sudo systemctl mask jupiter-biosupdate
 sudo steamos-readonly disable
 sudo mkdir -p /foxnet/bios/
 sudo touch /foxnet/bios/INHIBIT
