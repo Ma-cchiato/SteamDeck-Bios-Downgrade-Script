9ad76dae3faceb1698fdd5a992bc5a1180586794
Ma-cchiato <122413511+Ma-cchiato@users.noreply.github.com>
2023-11-01 16:58:06 +0900

    Update enable_bios_update.sh
    Add to reboot after enabling bios update

--- enable_bios_update.sh
+++ enable_bios_update.sh
@@ -4,3 +4,5 @@ sudo systemctl unmask jupiter-biosupdate
 sudo steamos-readonly disable
 sudo rm -rf /foxnet
 sudo steamos-readonly enable
+echo $(zenity --width=180 --height=20 --warning --title="Reboot Required" --text "Proceed with the reboot to enable the Bios Update.")
+sudo reboot
