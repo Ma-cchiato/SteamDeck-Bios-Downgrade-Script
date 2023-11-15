0058019361e33b0242743ba52060eee9312470bd
Ma-cchiato <122413511+Ma-cchiato@users.noreply.github.com>
2023-11-15 22:05:16 +0900

    Update bios.sh
    add Model check logic (OLED models are blocked for now)

--- bios.sh
+++ bios.sh
@@ -2,6 +2,8 @@
 
 SD_Unlocker_File=/home/deck/SD_Unlocker
 Current_Bios_Version=`sudo dmidecode -s bios-version`
+apu_name=`sudo dmidecode -s system-family`
+processor_version=`sudo dmidecode -s processor-version`
 Backup_Bios_File_Name="bios_backup_$Current_Bios_Version.bin".$(date "+%y%m%d%H%M")
 Backup_Bios_File=/home/deck/$Backup_Bios_File_Name
 Jupiter_bios=/usr/share/jupiter_bios/
@@ -10,7 +12,8 @@ COLOR_1="\033[1;34m"
 COLOR_2="\033[1;31m"
 COLOR_END="\033[0m"
 
-# 0 - SD_Unlocker, 1 - 110 Bios, 2 - 116 Bios, 3 - 118 Bios
+
+# 0 - SD_Unlocker, 1 - 110 Bios, 2 - 116 Bios, 3 - 118 Bios, 4 - 119 Bios
 # https://gitlab.com/evlaV/jupiter-PKGBUILD#valve-official-steam-deck-jupiter-release-bios-database
 Link=("https://github.com/Ma-cchiato/deck_bios_downgrade/raw/main/SD_Unlocker" "https://gitlab.com/evlaV/jupiter-hw-support/-/raw/0660b2a5a9df3bd97751fe79c55859e3b77aec7d/usr/share/jupiter_bios/F7A0110_sign.fd"
 "https://gitlab.com/evlaV/jupiter-hw-support/-/raw/38f7bdc2676421ee11104926609b4cc7a4dbc6a3/usr/share/jupiter_bios/F7A0116_sign.fd"
@@ -20,47 +23,83 @@ Link=("https://github.com/Ma-cchiato/deck_bios_downgrade/raw/main/SD_Unlocker" "
 Bios_List=("F7A0110" "F7A0116" "F7A0118" "F7A0119")
 
 log() {
-    local log_msg="$1"
-    if [ "$log_enabled" = true ]; then
-        if [ -n "$log_File" ]; then
-            echo "$log_msg" >> "$log_File"
-        else
-            echo "$log_msg" >> /dev/null
-        fi
+local log_msg="$1"
+if [ "$log_enabled" = true ]; then
+	if [ -n "$log_File" ]; then
+        echo "$log_msg" >> "$log_File"
+    else
+        echo "$log_msg" >> /dev/null
     fi
+fi
+}
+
+log_enable() {
+
+log_enabled=true
+#log_File_Name="bios_downgrader.log.$(date "+%y%m%d%H%M")"
+log_File_Name="bios_downgrader.log"
+log_File="/home/deck/$log_File_Name"
+echo "logging start"
+echo -e "log file >> $log_File\n"
+touch $log_File
+#cat /dev/null > $log_File
+echo -e "\n===========================================" >> $log_File
+echo -e "\nbios downgrader log" >> $log_File
+echo -e "Run Time: $(date "+%Y-%m-%d %H:%M:%S")\n" >> $log_File
+}
+
+
+check_model() {
+if [ "$apu_name" == "Aerith" ] && [ "$processor_version" == "AMD Custom APU 0405" ]; then
+	echo -e "\nYour device is LCD model."
+	echo -e "Supported model\n"
+ 	log "Device is LCD model"
+else
+	echo -e "\nYour device is NOT LCD model."
+ 	echo -e "OLED models are not yet supported.\n"
+ 	log "Device is NOT LCD model"
+  	exit 0
+fi
 }
 
 # Function to perform BIOS backup
 perform_bios_backup() {
-    if [ ! -f $Backup_Bios_File ]; then
-		# 기존 바이오스 백업, 복구 가이드는 https://gall.dcinside.com/mgallery/board/view/?id=steamdeck&no=91558 참고
-		# If you're debugging, comment out the following lines (to save testing time)
-		sudo /usr/share/jupiter_bios_updater/h2offt $Backup_Bios_File -O
-		if [ -f $Backup_Bios_File ]; then
-			log "Bios Backup File: $Backup_Bios_File"
-			echo "바이오스 백업 파일 생성 완료 ===>" $Backup_Bios_File_Name
-			echo "바이오스 백업 파일 생성 완료 ===>" $Backup_Bios_File_Name
-		else
-			log "Failed to create Bios Backup File"
-			echo "바이오스 백업 파일 생성 실패"
-			echo "바이오스 백업 파일 생성 실패"
-		fi
+if [ ! -f $Backup_Bios_File ]; then
+# 기존 바이오스 백업, 복구 가이드는 https://gall.dcinside.com/mgallery/board/view/?id=steamdeck&no=91558 참고
+# If you're debugging, comment out the following lines (to save testing time)
+sudo /usr/share/jupiter_bios_updater/h2offt $Backup_Bios_File -O
+	if [ -f $Backup_Bios_File ]; then
+		log "Bios Backup File: $Backup_Bios_File"
+		echo "바이오스 백업 파일 생성 완료 ===>" $Backup_Bios_File_Name
+		echo "바이오스 백업 파일 생성 완료 ===>" $Backup_Bios_File_Name
+	else
+		log "Failed to create Bios Backup File"
+		echo "바이오스 백업 파일 생성 실패"
+		echo "바이오스 백업 파일 생성 실패"
 	fi
+fi
 }
 
 # Function to install SD Unlocker
 install_sd_unlocker() {
-	testv="F7A0119"
-	log "Bios Version: $Current_Bios_Version"
-	echo "Bios Version: $Current_Bios_Version"
-    if [ $Current_Bios_Version == ${Bios_List[0]} ]||[ $Current_Bios_Version == ${Bios_List[1]} ]; then
-		log "Install SD Unlocker"
-		sudo $SD_Unlocker_File
-		echo "SD_Unlocker is Installed"
-	else
-		log "Not install SD Unlocker (Bios Version is 118 or Up)"
-		echo "SD_Unlocker is Not Installed (Bios Version is 118 or Up)"
-	fi
+log "Bios Version: $Current_Bios_Version"
+echo "Bios Version: $Current_Bios_Version"
+if [ $Current_Bios_Version == ${Bios_List[0]} ]||[ $Current_Bios_Version == ${Bios_List[1]} ]; then
+	log "Install SD Unlocker"
+	sudo $SD_Unlocker_File
+	echo "SD_Unlocker is Installed"
+else
+	log "Not install SD Unlocker (Bios Version is not 110 or 116)"
+	echo "SD_Unlocker is Not Installed (Support Bios Version is 110, 116)"
+fi
+}
+
+help_command(){
+echo -e "\nAvailable options\n"
+echo -e "Bios Backup: -B, -b"
+echo -e "Check Model: -C, -c"
+echo -e "Install SD Unlocker: -S, -s"
+echo -e "Boot Bios Menu: -M, -m"
 }
 
 # Logging is disabled by default
@@ -68,27 +107,49 @@ log_enabled=false
 log_File=""
 
 # logging enable option is -l
-while getopts "lbs" opt; do
+while getopts "lbsmchBSMCH" opt; do
   case $opt in
     l)
-      log_enabled=true
-	  #log_File_Name="bios_downgrader.log.$(date "+%y%m%d%H%M")"
-	  log_File_Name="bios_downgrader.log"
-	  log_File="/home/deck/$log_File_Name"
-	  echo "logging start"
-	  echo -e "log file >> $log_File\n"
-	  touch $log_File
-	  #cat /dev/null > $log_File
-	  echo -e "\n===========================================" >> $log_File
-	  echo -e "\nbios downgrader log" >> $log_File
-	  echo -e "Run Time: $(date "+%Y-%m-%d %H:%M:%S")\n" >> $log_File
+      log_enable
       ;;
     b)
       perform_bios_backup
       exit 0
       ;;
+    B)
+      perform_bios_backup
+      exit 0
+      ;;
     s)
       install_sd_unlocker
+      exit 0
+      ;;
+    S)
+      install_sd_unlocker
+      exit 0
+      ;;
+	M)
+      sudo systemctl reboot --firmware-setup
+      exit 0
+      ;;
+	m)
+      sudo systemctl reboot --firmware-setup
+      exit 0
+      ;;
+    c)
+      check_model
+      exit 0
+      ;;
+    C)
+      check_model
+      exit 0
+      ;;
+    h)
+      help_command
+      exit 0
+      ;;
+    H)
+      help_command
       exit 0
       ;;
 	\?)
@@ -98,8 +159,11 @@ while getopts "lbs" opt; do
   esac
 done
 
+# Check if it is an LCD model
+check_model
+
 echo "           Select Bios Version" 
-echo "[1] " ${Bios_List[0]} " [2] " ${Bios_List[1]} " [3] " ${Bios_List[2]} #" [4] " ${Bios_List[3]} 
+echo "[1] " ${Bios_List[0]} " [2] " ${Bios_List[1]} " [3] " ${Bios_List[2]} " [4] " ${Bios_List[3]} 
 read -p "==> " select
 if [ $select == "1" ]; then
 	Bios_Version=${Bios_List[0]}
@@ -110,9 +174,9 @@ elif [ $select == "2" ]; then
 elif [ $select == "3" ]; then
 	Bios_Version=${Bios_List[2]}
 	log "$Bios_Version Bios select" 
-#elif [ $select == "4" ]; then
-#	Bios_Version=${Bios_List[3]}
-#	log "$Bios_Version Bios select"
+elif [ $select == "4" ]; then
+	Bios_Version=${Bios_List[3]}
+	log "$Bios_Version Bios select"
 else
 	log "Decline Bios Select: $select" 
 	echo "Process terminated"
@@ -156,9 +220,9 @@ for ((try = 1; try <= max_download_try; try++)); do
         elif [ "$Bios_Version" == "${Bios_List[2]}" ]; then
             log "Downloading Bios File ==> $Bios_File"
 			wget "${Link[3]}" -O "$Bios_File"
-        #elif [ "$Bios_Version" == "${Bios_List[3]}" ]; then
-        #    log "Downloading Bios File ==> $Bios_File"
-		#	wget "${Link[4]}" -O "$Bios_File"
+        elif [ "$Bios_Version" == "${Bios_List[3]}" ]; then
+            log "Downloading Bios File ==> $Bios_File"
+			wget "${Link[4]}" -O "$Bios_File"
         fi
         if [ -f "$Bios_File" ] && [ $(stat -c %s "$Bios_File") -eq $Bios_Size ]; then
             downloaded=true
@@ -215,11 +279,11 @@ elif [ $Bios_Version == ${Bios_List[2]} ]; then
 	log "Copying one files"
 	log "$(sudo ls -l /usr/share/jupiter_bios/F7A*.fd)"
 	#echo "Not Copied Bios File"
-#elif [ $Bios_Version == ${Bios_List[3]} ]; then
-#	sudo cp $Bios_File $Jupiter_bios${Bios_List[3]}"_sign.fd" # For SteamOS 3.6 
-#	echo "Copy "${Bios_List[3]} "Bios File to jupiter_bios ===> "${Bios_List[3]}"_sign.fd"
-#	log "Copying one files"
-#	log "$(sudo ls -l /usr/share/jupiter_bios/F7A*.fd)"
+elif [ $Bios_Version == ${Bios_List[3]} ]; then
+	sudo cp $Bios_File $Jupiter_bios${Bios_List[3]}"_sign.fd" # For SteamOS 3.6 
+	echo "Copy "${Bios_List[3]} "Bios File to jupiter_bios ===> "${Bios_List[3]}"_sign.fd"
+	log "Copying one files"
+	log "$(sudo ls -l /usr/share/jupiter_bios/F7A*.fd)"
 	#echo "Not Copied Bios File"
 fi
 sudo steamos-readonly enable
@@ -228,6 +292,6 @@ log "Steam OS Read Only: Enable"
 install_sd_unlocker
 
 log "$(sudo /usr/share/jupiter_bios_updater/h2offt -SC)"
-log "Update Bios Information.. [ Update to $Bios_Version, Use File: $Bios_File ]"
+log "Bios information to update.. [ Update to $Bios_Version, Use File: $Bios_File ]"
 # If you're debugging, comment out the following lines (to save testing time)
 sudo /usr/share/jupiter_bios_updater/h2offt $Bios_File
